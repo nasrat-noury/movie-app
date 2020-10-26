f332139fd21e8d03595e4b838a5011e7d37b3f80
nasrat <nasrat.noury@gmail.com>
2020-10-26 14:06:02 +0100

    first rep

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import MovieForm from "./MovieForm";
+
+function App() {
+  return (
+    <div className="App">
+      <MovieForm />
+    </div>
+  );
+}
+
+export default App;
